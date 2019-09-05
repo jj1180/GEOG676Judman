67b8edba9df043b28ce9559e07d75d09dfdef035
jj1180 <54554245+jj1180@users.noreply.github.com>
2019-09-05 14:57:10 -0500

    Add files via upload

---
+++ Lab/Week02/Lab_2_Part3.py
@@ -0,0 +1,6 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> myList = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21]
+>>> sum(num for num in myList if not num%2)
+458
+>>> 
