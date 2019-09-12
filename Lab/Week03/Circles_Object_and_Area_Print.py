7f4a8983bc637a02476338b7931f09c3adb83049
jj1180 <54554245+jj1180@users.noreply.github.com>
2019-09-12 09:21:45 -0500

    Add files via upload

---
+++ Lab/Week03/Circles_Object_and_Area_Print.py
@@ -0,0 +1,20 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def getArea(r):
+
+    # calculate the area
+    Area = r ** 2 * 3.14
+
+    print("\n getArea is: %.2f" %Area)
+
+    
+>>> getArea (3)
+
+ getArea is: 28.26
+>>> getArea (3)
+
+ getArea is: 28.26
+>>> getArea (9)
+
+ getArea is: 254.34
+>>> 
