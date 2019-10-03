9a659e455747bad73b20c71c1d5ca8e46a4c458e
jj1180 <54554245+jj1180@users.noreply.github.com>
2019-10-03 16:38:37 -0500

    Add files via upload

---
+++ Lab/Week07/Composite_Code.py
@@ -0,0 +1,8 @@
+﻿# coding: utf-8
+import arcpy
+source = r"C:/output/KyleField.tif/"
+band1 = arcpy.sa.Raster(source + "Band_1") # red
+band2 = arcpy.sa.Raster(source + "Band_2") # green
+band3 = arcpy.sa.Raster(source + "Band_3") # blue
+band4 = arcpy.sa.Raster(source + "Band_4") # NIR
+composite = arcpy.CompositeBands_management([band1, band2, band3, band4], source + "KyleField_combined.tif")
---
+++ Lab/Week07/Slope_Code.py
@@ -0,0 +1,12 @@
+﻿# coding: utf-8
+import arcpy
+from arcpy import env
+env.workspace = "C:/output"
+inRaster = "C:/output/CollegeStationElevation.tif"
+outRaster = "C:/output/SlopeCS"
+outMeasurement = "DEGREE"
+zFactor = ""
+method = "GEODESIC"
+zUnit = "METER"
+arcpy.Slope_3d(inRaster, outRaster, outMeasurement, zFactor, method, zUnit)
+# <Result 'C:\\output\\SlopeCS'>
